4e553d6b8ce1023b4b3eebd95ef59e82cee2c936
Shunsuke Fujinawa <shun26497204@gmail.com>
2024-12-31 18:25:13 +0900

    add: ログインユーザーの作成したアイテムだけを更新・削除できるようトークン判定処理を追加

--- app/api/item/delete/[id]/route.js
+++ app/api/item/delete/[id]/route.js
@@ -3,10 +3,16 @@ import { ItemModel } from "@/app/utils/schemaModels";
 import { NextResponse } from "next/server";
 
 export async function DELETE(request, context) {
+  const reqBody = await request.json();
   try {
     await connectDB();
-    await ItemModel.deleteOne({ _id: context.params.id });
-    return NextResponse.json({ message: "アイテム削除" });
+    const singleItem = await ItemModel.findById(context.params.id);
+    if (singleItem.email === reqBody.email) {
+      await ItemModel.deleteOne({ _id: context.params.id });
+      return NextResponse.json({ message: "アイテム削除成功" });
+    } else {
+      return NextResponse.json({ message: "他の人が作成したアイテムです" });
+    }
   } catch (error) {
     return NextResponse.json({ message: "エラー" });
   }
--- app/api/item/update/[id]/route.js
+++ app/api/item/update/[id]/route.js
@@ -6,8 +6,15 @@ export async function PUT(request, context) {
   const reqBody = await request.json();
   try {
     await connectDB();
-    await ItemModel.updateOne({ _id: context.params.id }, reqBody);
-    return NextResponse.json({ message: "アイテム更新" });
+    const singleItem = await ItemModel.findById(context.params.id);
+
+    // アイテム作成者と同じメールアドレスの含まれたトークンを保持するユーザーだけが更新できる
+    if (singleItem === reqBody.email) {
+      await ItemModel.updateOne({ _id: context.params.id }, reqBody);
+      return NextResponse.json({ message: "更新成功" });
+    } else {
+      return NextResponse.json({ message: "他の人が作成したアイテムです" });
+    }
   } catch (error) {
     return NextResponse.json({ message: "エラー" });
   }
