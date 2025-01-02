84556671875c60b4d33cffd9329890698489e25f
Shunsuke Fujinawa <shun26497204@gmail.com>
2025-01-02 15:44:42 +0900

    add: client初期画面でアイテムを全取得し表示するよう実装

--- app/page.js
+++ app/page.js
@@ -1,7 +1,34 @@
-export default function Home() {
+import Image from "next/image";
+import Link from "next/link";
+
+const ReadAllItems = async () => {
+  /** サーバーからアイテム全取得 */
+  const getAllItems = async () => {
+    const response = await fetch("http://localhost:3000/api/item/readall");
+    const jsonData = await response.json();
+    return jsonData.allItems;
+  }
+
+  const allItems = await getAllItems();
+  console.log(allItems);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold text-center text-white">Welcome to Next.js!</h1>
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <ul>
+        {allItems.map((item, index) => (
+          <li key={item._id}>
+            <Link href={`/item/readsingle/${item._id}`}>
+              <Image width={750} height={500} src={item.image} alt="商品写真" />
+              <h2>{ item.price }</h2>
+              <h3>{ item.title }</h3>
+              <p>{ item.description.substring(0, 80) }...</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export default ReadAllItems;
